52e28273c67ecdbc07263aefad7cfe1354717910
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-20 22:25:15 +0800

    event binding

---
+++ src/components/EventBind.js
@@ -0,0 +1,16 @@
+// event binding
+
+import React, { Component } from 'react';
+
+class EventBind extends Component{
+
+    render(){
+        return (
+            <div>
+                <button>Bind Me</button>
+            </div>
+        )
+    }
+}
+
+export default EventBind;
