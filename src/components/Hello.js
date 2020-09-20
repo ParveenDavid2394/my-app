638492d275ac96db70d5318ba1725439bea1dc9b
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-20 17:45:27 +0800

    added id and class attribute

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -3,13 +3,21 @@
 import React from 'react';
 
 // example utilising JSX
-const Hello = () => {
+// const Hello = () => {
+//     return (
+//         <div>
+//             <h1>
+//                 Hello Marco using JSX
+//             </h1>
+//         </div>
+//     );
+// }
+
+// example without using JSX
+const Hello = () =>{
     return (
-        <div>
-            <h1>
-                Hello Marco using JSX
-            </h1>
-        </div>
+        React.createElement('div', { id: 'div-id', className: 'div-class'}, 
+        React.createElement('h1', null, 'Hello Marco without JSX'))
     );
 }
 
