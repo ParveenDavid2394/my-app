d636bbacc05a97eeb86ab0678deb447882b9c84b
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-20 16:25:06 +0800

    added class component

---
+++ src/components/Welcome.js
@@ -0,0 +1,8 @@
+// example of stateful class component
+import React, { Component } from 'react';
+
+class Welcome extends Component{
+    render(){
+        return <h1>This is a Class Component</h1>
+    }
+}
