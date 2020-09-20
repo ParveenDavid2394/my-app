3b688876d3221ca2e75d8536aefa0aba81890086
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-20 17:37:04 +0800

    example using JSX

---
+++ src/components/Hello.js
@@ -0,0 +1,14 @@
+// simple example of code with and without JSX
+
+import React from 'react';
+
+// example utilising JSX
+const Hello = () => {
+    return (
+        <div>
+            <h1>
+                Hello Marco using JSX
+            </h1>
+        </div>
+    );
+}
