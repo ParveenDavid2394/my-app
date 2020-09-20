32649ced93c659755ce1fc0c616166deb727de82
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-20 22:06:02 +0800

    event handling

---
+++ src/components/FunctionClick.js
@@ -0,0 +1,19 @@
+// event handling
+
+import React from 'react';
+
+function FunctionClick(){
+
+    // function to handle click event
+    function clickHandler(){
+        console.log('Button Clicked');
+    }
+
+    return(
+        <div>
+            <button onClick={ clickHandler() }>Click</button>
+        </div>
+    )
+}
+
+export default FunctionClick;
