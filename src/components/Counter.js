80eda15acafee478afa4fce9e0ee64fadb498cd8
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-20 21:25:35 +0800

    learned setState

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,54 @@
 // setState
+/*
+    never modify the state directly. if want to modify the state outside of constructor
+    method, use setState
+
+    setState works asynchronously , so if there's any method that needs to execute right after
+    state is change, place them as callback functions in the setState method.
+
+    if need to update the state based on previous state, use a function in setState instead of
+    an object
+*/
 
 import React, { Component } from 'react';
 
 class Counter extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            count: 0
+        }
+    }
+
+    increment(){
+        // this.setState(
+        //     {
+        //     count: this.state.count + 1
+        // }, 
+        // () => { console.log('Callback function val: ' + this.state.count)}
+        
+        // )
+
+        this.setState( (prevState) => ({
+            count: prevState.count + 1
+        }))
+    }
+
+    incrementFive(){
+        this.increment();
+        this.increment();
+        this.increment();
+        this.increment();
+        this.increment();
+    }
+
     render(){
         return (
             <div>
-                count
+                <div> count - {this.state.count} </div>
+                <button onClick={() => this.incrementFive()}>Increment</button>
             </div>
         );
     }
