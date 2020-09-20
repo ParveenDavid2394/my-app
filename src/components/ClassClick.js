f6f048e771a29f5727466a75bcd27e255075ad52
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-20 22:13:08 +0800

    event handling in class

---
+++ src/components/ClassClick.js
@@ -0,0 +1,20 @@
+// event handling in class
+
+import React, { Component } from 'react';
+
+class ClassClick extends Component{
+
+    clickHandler(){
+        console.log('Button is clicked');
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={ this.clickHandler }>Click Me</button>
+            </div>
+        )
+    }
+}
+
+export default ClassClick;
