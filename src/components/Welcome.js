3778f24db29423505d7ceae4d7bad7bc782f1044
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-20 21:32:58 +0800

    destructuring props and state in class

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,10 +3,16 @@ import React, { Component } from 'react';
 
 class Welcome extends Component{
     render(){
+        // destructuring props in class
+        const { name, heroName} = this.props;
+
+        // destructuring state in class
+        //const { state1, state2 } = this.state;
+        
         return(
             <div>
                 <h1>This is a Class Component</h1>
-                <h1>Welcome {this.props.name} a.k.a {this.props.heroName} </h1>
+                <h1>Welcome {name} a.k.a {heroName} </h1>
             </div>
         )
     }
