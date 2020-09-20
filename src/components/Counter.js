c66bf81b20295319c933340c88769b73813d5c00
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-20 20:42:48 +0800

    setState example

---
+++ src/components/Counter.js
@@ -0,0 +1,15 @@
+// setState
+
+import React, { Component } from 'react';
+
+class Counter extends Component{
+    render(){
+        return (
+            <div>
+                count
+            </div>
+        );
+    }
+}
+
+export default Counter;
