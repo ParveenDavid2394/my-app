6f49bc4c050315ee353d1f7c11da80c93a31d58e
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-24 10:01:46 +0800

    usage of portal in React

--- src/components/PortalDemo.js
+++ src/components/PortalDemo.js
@@ -1,16 +1,17 @@
 /*
     In React all components are rendered in div with class='root' 
 
-    Portal allows us to render component in divs other than the default one
+    Portal allows us to render component in DOM nodes other than the default one
     with class='root'
  */
 
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 function PortalDemo(){
-    return React.createPortal(
+    return ReactDOM.createPortal(
         <div>
-            Portal Demo
+            <h1>Portal Demo</h1>
         </div>,
         document.getElementById('portal-root')
     )
